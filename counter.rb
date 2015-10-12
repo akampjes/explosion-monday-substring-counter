4480968d798c8a40e6b436301aab07e3f0fdada7
Andrew Kampjes <andrew.kampjes@powershop.co.nz>
2015-10-12 14:01:16 +1300

    Split out counter into methods

--- counter.rb
+++ counter.rb
@@ -1,26 +1,34 @@
+def count_occurances_in_buffer(substring, buffer)
+  occurances = 0
+  buffer_start = 0
+
+  while true
+    location_found = buffer.index(substring, buffer_start)
+    break if location_found.nil?
+
+    buffer_start = location_found + 1
+    occurances += 1
+  end
+
+  occurances
+end
+
+def count_occurances_in_file(substring, fin, max_buffersize)
+  occurances_count = 0
+  file_buffer = fin.read(max_buffersize)
+  while file_buffer
+    occurances_count += count_occurances_in_buffer(substring, file_buffer)
+
+    file_buffer = fin.read(max_buffersize)
+  end
+
+  occurances_count
+end
+
 MAX_BUFFERSIZE = 1024 * 1024
 
 substring = ARGV[0]
 file_name = ARGV[1]
 fin = File.open(file_name, 'rb')
 
-occurances_count = 0
-upto = 0
-file_buffer = fin.read(MAX_BUFFERSIZE)
-while file_buffer
-  buffer_start = 0
-
-  while true
-    location_found = file_buffer.index(substring, buffer_start)
-    break if location_found.nil?
-
-    occurances_count += 1
-    buffer_start = location_found + 1
-  end
-
-  upto += MAX_BUFFERSIZE - substring.length
-  fin.seek(upto)
-  file_buffer = fin.read(MAX_BUFFERSIZE)
-end
-
-puts occurances_count
+puts count_occurances_in_file(substring, fin, MAX_BUFFERSIZE)
