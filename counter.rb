747836a515fb9f44e291f1c82b44743d6a007194
Andrew Kampjes <andrew.kampjes@powershop.co.nz>
2015-10-12 16:47:36 +1300

    Final answer

--- counter.rb
+++ counter.rb
@@ -1,21 +1,29 @@
 def count_occurances_in_file(substring, fin, max_buffersize)
   occurances_count = 0
   seek_to = 0
-  file_buffer = fin.read(max_buffersize)
-  while file_buffer
-    
+  file_buffer = ''
+  
+  # define these now, optimization yo
+  buffer_start = 0
+  substr_len = substring.length
+  last_match_end = 0
+  new_buf_start = 0
+  last_location = nil
+  location_found = 0
+
+  while fin.read(max_buffersize, file_buffer)
     buffer_start = 0
+
     while true
       location_found = file_buffer.index(substring, buffer_start)
-      break if location_found.nil?
+      break if !location_found
       last_location = location_found
-
-      buffer_start = location_found + substring.length
+      buffer_start = location_found + substr_len
       occurances_count += 1
     end
 
-    last_match_end = seek_to + last_location + substring.length
-    new_buf_start = seek_to + max_buffersize - substring.length + 1
+    last_match_end = seek_to + (last_location || 0) + substr_len
+    new_buf_start = seek_to + max_buffersize - substr_len + 1
 
     if last_match_end > new_buf_start
       seek_to = last_match_end
@@ -24,7 +32,6 @@ def count_occurances_in_file(substring, fin, max_buffersize)
     end
 
     fin.seek(seek_to)
-    file_buffer = fin.read(max_buffersize)
   end
 
   occurances_count
